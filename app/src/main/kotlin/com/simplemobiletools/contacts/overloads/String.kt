9c44837d0c814dcb7fb006bbf5db138af7d5780e
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2017-12-26 01:44:21 +0300

    Use StringBuilder in String.times()
    Has better performance than StringBuffer

--- app/src/main/kotlin/com/simplemobiletools/contacts/overloads/String.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/overloads/String.kt
@@ -1,9 +1,9 @@
 package com.simplemobiletools.contacts.overloads
 
 operator fun String.times(x: Int): String {
-    val stringBuffer = StringBuffer()
+    val sb = StringBuilder()
     for (i in 1..x) {
-        stringBuffer.append(this)
+        sb.append(this)
     }
-    return stringBuffer.toString()
+    return sb.toString()
 }
