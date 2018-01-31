2c5daff95c4defe266164238a570764c67f67eb3
tibbi <tibor@kaputa.sk>
2018-01-31 18:04:26 +0100

    add some null checks to filtering contacts adapter entries at search

--- app/src/main/kotlin/com/simplemobiletools/contacts/fragments/MyViewPagerFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/fragments/MyViewPagerFragment.kt
@@ -187,7 +187,7 @@ abstract class MyViewPagerFragment(context: Context, attributeSet: AttributeSet)
     }
 
     fun onSearchOpened() {
-        contactsIgnoringSearch = (fragment_list.adapter as? ContactsAdapter)?.contactItems as ArrayList
+        contactsIgnoringSearch = (fragment_list?.adapter as? ContactsAdapter)?.contactItems as? ArrayList ?: ArrayList()
     }
 
     fun onSearchClosed() {
