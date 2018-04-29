6d7e28a3b647e446e3d2cc6a220a9d243e9a023d
tibbi <tibor@kaputa.sk>
2018-04-29 09:59:00 +0200

    update commons to 3.20.5 with some new icons

--- app/src/main/kotlin/com/simplemobiletools/contacts/activities/SimpleActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/activities/SimpleActivity.kt
@@ -1,5 +1,13 @@
 package com.simplemobiletools.contacts.activities
 
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import com.simplemobiletools.contacts.R
 
-open class SimpleActivity : BaseSimpleActivity()
+open class SimpleActivity : BaseSimpleActivity() {
+    override fun getAppIconIDs() = arrayListOf(
+            R.mipmap.ic_launcher_red,
+            R.mipmap.ic_launcher
+    )
+
+    override fun getAppLauncherName() = getString(R.string.app_launcher_name)
+}
