96c6e8cd5a3edfc026ca2a12e142770e584db140
tibbi <tibor@kaputa.sk>
2018-09-23 10:23:23 +0200

    add a dialpad input for showing dialed chars

--- app/src/main/kotlin/com/simplemobiletools/contacts/activities/DialpadActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/activities/DialpadActivity.kt
@@ -9,22 +9,27 @@ class DialpadActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialpad)
-        dialpad_0.setOnClickListener { }
-        dialpad_1.setOnClickListener { }
-        dialpad_2.setOnClickListener { }
-        dialpad_3.setOnClickListener { }
-        dialpad_4.setOnClickListener { }
-        dialpad_5.setOnClickListener { }
-        dialpad_6.setOnClickListener { }
-        dialpad_7.setOnClickListener { }
-        dialpad_8.setOnClickListener { }
-        dialpad_9.setOnClickListener { }
-        dialpad_asterisk.setOnClickListener { }
-        dialpad_hashtag.setOnClickListener { }
+
+        dialpad_0.setOnClickListener { dialpadPressed("0") }
+        dialpad_1.setOnClickListener { dialpadPressed("1") }
+        dialpad_2.setOnClickListener { dialpadPressed("2") }
+        dialpad_3.setOnClickListener { dialpadPressed("3") }
+        dialpad_4.setOnClickListener { dialpadPressed("4") }
+        dialpad_5.setOnClickListener { dialpadPressed("5") }
+        dialpad_6.setOnClickListener { dialpadPressed("6") }
+        dialpad_7.setOnClickListener { dialpadPressed("7") }
+        dialpad_8.setOnClickListener { dialpadPressed("8") }
+        dialpad_9.setOnClickListener { dialpadPressed("9") }
+        dialpad_asterisk.setOnClickListener { dialpadPressed("*") }
+        dialpad_hashtag.setOnClickListener { dialpadPressed("#") }
     }
 
     override fun onResume() {
         super.onResume()
         updateTextColors(dialpad_wrapper)
+    }
+
+    private fun dialpadPressed(char: String) {
+        dialpad_input.append(char)
     }
 }
