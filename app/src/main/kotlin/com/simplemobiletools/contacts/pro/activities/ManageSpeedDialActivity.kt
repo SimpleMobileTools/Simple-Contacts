23cdc2bd5485e122e7eb96a231c04f57da28e7c5
tibbi <tibor@kaputa.sk>
2020-01-17 11:54:16 +0100

    adding the top label at speed dial screen

--- app/src/main/kotlin/com/simplemobiletools/contacts/pro/activities/ManageSpeedDialActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/pro/activities/ManageSpeedDialActivity.kt
@@ -1,5 +1,11 @@
 package com.simplemobiletools.contacts.pro.activities
 
-class ManageSpeedDialActivity : SimpleActivity() {
+import android.os.Bundle
+import com.simplemobiletools.contacts.pro.R
 
+class ManageSpeedDialActivity : SimpleActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_speed_dial)
+    }
 }
