65610e6fd46ac6bb452a74ec0d698d67184e08a8
tibbi <tibor@kaputa.sk>
2020-05-31 10:31:29 +0200

    moving the start_name_with_surname config into commons

--- app/src/main/kotlin/com/simplemobiletools/contacts/pro/helpers/Config.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/pro/helpers/Config.kt
@@ -24,10 +24,6 @@ class Config(context: Context) : BaseConfig(context) {
         get() = prefs.getBoolean(SHOW_ONLY_CONTACTS_WITH_NUMBERS, false)
         set(showOnlyContactsWithNumbers) = prefs.edit().putBoolean(SHOW_ONLY_CONTACTS_WITH_NUMBERS, showOnlyContactsWithNumbers).apply()
 
-    var startNameWithSurname: Boolean
-        get() = prefs.getBoolean(START_NAME_WITH_SURNAME, false)
-        set(startNameWithSurname) = prefs.edit().putBoolean(START_NAME_WITH_SURNAME, startNameWithSurname).apply()
-
     var lastUsedContactSource: String
         get() = prefs.getString(LAST_USED_CONTACT_SOURCE, "")!!
         set(lastUsedContactSource) = prefs.edit().putString(LAST_USED_CONTACT_SOURCE, lastUsedContactSource).apply()
