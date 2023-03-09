1e4fd30949001df5407a14e01ef754220f92edeb
ismailnurudeen <ibrightstar247@gmail.com>
2023-03-09 13:09:18 +0100

    fix: move show tab pref to own config

--- app/src/main/kotlin/com/simplemobiletools/contacts/pro/helpers/Config.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/pro/helpers/Config.kt
@@ -2,9 +2,14 @@ package com.simplemobiletools.contacts.pro.helpers
 
 import android.content.Context
 import com.simplemobiletools.commons.helpers.BaseConfig
+import com.simplemobiletools.commons.helpers.SHOW_TABS
 
 class Config(context: Context) : BaseConfig(context) {
     companion object {
         fun newInstance(context: Context) = Config(context)
     }
+
+    var showTabs: Int
+        get() = prefs.getInt(SHOW_TABS, ALL_TABS_MASK)
+        set(showTabs) = prefs.edit().putInt(SHOW_TABS, showTabs).apply()
 }
