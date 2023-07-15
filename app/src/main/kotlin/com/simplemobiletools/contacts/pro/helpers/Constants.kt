5120ba7863150913d0233cc48e540defb68b860d
merkost <konstantinlikes@gmail.com>
2023-07-15 13:50:09 +1000

    Added backup settings

--- app/src/main/kotlin/com/simplemobiletools/contacts/pro/helpers/Constants.kt
+++ app/src/main/kotlin/com/simplemobiletools/contacts/pro/helpers/Constants.kt
@@ -3,6 +3,7 @@ package com.simplemobiletools.contacts.pro.helpers
 import com.simplemobiletools.commons.helpers.TAB_CONTACTS
 import com.simplemobiletools.commons.helpers.TAB_FAVORITES
 import com.simplemobiletools.commons.helpers.TAB_GROUPS
+import org.joda.time.DateTime
 
 const val GROUP = "group"
 const val IS_FROM_SIMPLE_CONTACTS = "is_from_simple_contacts"
@@ -12,6 +13,16 @@ const val FIRST_GROUP_ID = 10000L
 const val DEFAULT_FILE_NAME = "contacts.vcf"
 const val AVOID_CHANGING_TEXT_TAG = "avoid_changing_text_tag"
 const val AVOID_CHANGING_VISIBILITY_TAG = "avoid_changing_visibility_tag"
+
+const val AUTOMATIC_BACKUP_REQUEST_CODE = 10001
+const val AUTO_BACKUP_INTERVAL_IN_DAYS = 1
+
+const val AUTO_BACKUP = "auto_backup"
+const val AUTO_BACKUP_FOLDER = "auto_backup_folder"
+const val AUTO_BACKUP_FILENAME = "auto_backup_filename"
+const val LAST_AUTO_BACKUP_TIME = "last_auto_backup_time"
+
+const val AUTO_BACKUP_CONTACT_SOURCES = "auto_backup_contact_sources"
 
 // extras used at third party intents
 const val KEY_NAME = "name"
@@ -53,3 +64,20 @@ const val SIGNAL = "signal"
 const val VIBER = "viber"
 const val TELEGRAM = "telegram"
 const val THREEMA = "threema"
+
+
+// 6 am is the hardcoded automatic backup time, intervals shorter than 1 day are not yet supported.
+fun getNextAutoBackupTime(): DateTime {
+    val now = DateTime.now()
+    val sixHour = now.withHourOfDay(6)
+    return if (now.millis < sixHour.millis) {
+        sixHour
+    } else {
+        sixHour.plusDays(AUTO_BACKUP_INTERVAL_IN_DAYS)
+    }
+}
+
+fun getPreviousAutoBackupTime(): DateTime {
+    val nextBackupTime = getNextAutoBackupTime()
+    return nextBackupTime.minusDays(AUTO_BACKUP_INTERVAL_IN_DAYS)
+}
